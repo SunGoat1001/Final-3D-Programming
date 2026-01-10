e1092bfd4b8b0ca8f6e085ecf1d73718a618a75c
SunGoat1001 <ngoctien10012004@gmail.com>
2026-01-10 16:25:35 +0700

    config vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: process.env.VITE_BASE_PATH || '/',
+    base: process.env.VITE_BASE_PATH || '/Final-3D-Programming/',
     build: {
         rollupOptions: {
             input: {
